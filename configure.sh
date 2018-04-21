b91b2d1a08abd3b233fbfb263f2aa6af0e4fcd93
Nicholas Wolf <nwolf@levelupsystem.com>
2018-04-21 04:38:31 -0700

    Initial commit

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a setup script for configuring the default Manjaro i3 installation.
+
+cp -i ./config/i3/config ~/.i3/config
+cp -ir ./config/polybar ~/.config/
---
+++ install_software.sh
@@ -0,0 +1,13 @@
+# Update pacman databases
+sudo pacman -Syy
+
+# Web browsing software
+sudo pacman -S \
+	firefox
+
+# Install polybar and dependencies
+sudo pacman -S \
+	libmpdclient \
+	jsoncpp \
+	polybar
+
